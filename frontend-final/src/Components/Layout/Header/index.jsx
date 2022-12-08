66130792bfa340a2010fe4cb0aa35c5dceab23df
fedeargfyron <fedemgs15@gmail.com>
2022-12-08 16:31:38 -0300

    Se modifica encabezado según usuario logueado o deslogueado

--- frontend-final/src/Components/Layout/Header/index.jsx
+++ frontend-final/src/Components/Layout/Header/index.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom'
 import { logOutUser } from '../../../Store/users/actions';
+import Button from '../../Shared/Button';
 import styles from './header.module.css'
 const Header = () => {
+  const user = localStorage.getItem('user');
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const logIn = () => {
+    navigate('/login');
+  }
+
   const logOut = () => {
     dispatch(logOutUser());
     localStorage.removeItem("user");
@@ -16,7 +22,19 @@ const Header = () => {
         <div>
             <h1 className={styles.title}>Arrón Final</h1>
         </div>
-        <button onClick={() => logOut()}>Log out</button>
+        { user ? <Button
+          onClick={logOut}
+          background='none'
+          color='white'
+          content='Log out'
+          />
+        : <Button
+        onClick={logIn}
+        background='none'
+        color='white'
+        content='Log in'
+        />}
+        
     </header>
   );
 }
